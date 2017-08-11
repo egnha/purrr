858b809bb48e8e0db06dac2f30d1d3a7916fa1ca
Eugene Ha <eha@posteo.de>
2017-08-11 08:20:15 +0200

    Test of new functionality of compose()

--- tests/testthat/test-compose.R
+++ tests/testthat/test-compose.R
@@ -7,3 +7,41 @@ test_that("composed functions are applied right to left", {
   x <- sample(1:4, 100, replace = TRUE)
   expect_identical(unname(sort(table(x))), compose(unname, sort, table)(x))
 })
+
+test_that("formals of composition matches formals of first function called", {
+  foo <- function(x, y, ..., z = "z") NULL
+  bar <- function(...) NULL
+  baz <- function(a) NULL
+  expect_identical(formals(compose(baz, bar, foo)), formals(foo))
+})
+
+test_that("first called function is called in calling environment", {
+  expect_identical(
+    compose(identity, function() parent.frame())(),
+    environment()
+  )
+})
+
+test_that("function-formulas are indeed interpreted as functions", {
+  cmp1 <- compose(identity, ~ . + 1)
+  cmp2 <- compose(~ . + 1, identity)
+  for (x in runif(10))
+    expect_equal(cmp1(x), x + 1)
+})
+
+test_that("list of functions can be spliced", {
+  fns <- list(log, abs, `+`)
+  compose(log, abs, `+`) %>%
+    expect_equal(compose(!!! fns)) %>%
+    expect_equal(compose(UQS(fns)))
+})
+
+test_that("composite function can be decomposed", {
+  f <- compose(identity, log, abs, `+`)
+  expect_equivalent(decompose(f), list(identity, log, abs, `+`))
+})
+
+test_that("compose() inverts decompose() (upon splicing)", {
+  f <- compose(identity, log, abs, `+`)
+  expect_equal(compose(!!! decompose(f)), f)
+})
